19cab1060ed32b89eb7e97bbfb00127cae7570cf
tiffsea <tseeley@mail.usf.edu>
2020-03-03 04:10:07 +0000

    added app files

---
+++ App/screens/HomeScreen.js
@@ -0,0 +1,178 @@
+import * as React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import * as WebBrowser from 'expo-web-browser';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={ 
+              require('../assets/images/house-logo2.png')
+            }
+            style={styles.welcomeImage}
+          />
+        </View>
+
+        <View style={styles.getStartedContainer}>
+          <DevelopmentModeNotice />
+
+          <Text style={styles.getStartedText}>This application was created using </Text>
+
+          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+            <MonoText>react-native</MonoText>
+          </View>
+
+          <Text style={styles.getStartedText}>
+            and
+          </Text>
+          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+            <MonoText>react-native-maps</MonoText>
+          </View>
+        </View>
+
+        <View style={styles.helpContainer}>
+          <TouchableOpacity onPress={handleRepoPress} style={styles.helpLink}>
+            <Text style={styles.helpLinkText}>Check out the repo on Github</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.tabBarInfoText}>Home</Text>
+      </View>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+function DevelopmentModeNotice() {
+  if (__DEV__) {
+    const learnMoreButton = (
+      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
+        click here
+      </Text>
+    );
+
+    return (
+      <Text style={styles.developmentModeText}>
+        Welcome to a UK House Prices Map visualisation.
+        Use the navigation options at the botton of the screen to view the interactive map.
+        To view the UK Goverment data files directly, {learnMoreButton}.
+        Due to limited development time, this map visualisation refers to a small sample size of UK house prices. 
+      </Text>
+    );
+  } else {
+    return (
+      <Text style={styles.developmentModeText}>
+        You are not in development mode: your app will run at full speed.
+      </Text>
+    );
+  }
+}
+
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync('https://www.gov.uk/government/statistical-data-sets/price-paid-data-downloads#single-file');
+}
+
+function handleRepoPress() {
+  WebBrowser.openBrowserAsync(
+    'https://github.com/tiffsea/Advanced-Software-Engineering'
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
---
+++ App/screens/LinksScreen.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+
+export default function LinksScreen() {
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <OptionButton
+        icon="md-school"
+        label="University of Sussex Advanced Software Engineering Postgraduate Module Fall 2019"
+        onPress={() => WebBrowser.openBrowserAsync('http://users.sussex.ac.uk/~mfb21/ase/material.html')}
+      />
+
+      <OptionButton
+        icon="logo-github"
+        label="Code Repositories"
+        onPress={() => WebBrowser.openBrowserAsync('https://github.com/tiffsea')}
+      />
+
+      <OptionButton
+        icon="ios-chatboxes"
+        label="Ask a question or raise an issue"
+        onPress={() => WebBrowser.openBrowserAsync('https://github.com/tiffsea/Advanced-Software-Engineering/issues')}
+      />
+
+      <OptionButton
+        icon="ios-chatboxes"
+        label="tiffseeley@gmail.com"
+        isLastOption
+      />
+    </ScrollView>
+  );
+}
+
+function OptionButton({ icon, label, onPress, isLastOption }) {
+  return (
+    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.optionIconContainer}>
+          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+        </View>
+        <View style={styles.optionTextContainer}>
+          <Text style={styles.optionText}>{label}</Text>
+        </View>
+      </View>
+    </RectButton>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  contentContainer: {
+    paddingTop: 15,
+  },
+  optionIconContainer: {
+    marginRight: 12,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: '#ededed',
+  },
+  lastOption: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  optionText: {
+    fontSize: 15,
+    alignSelf: 'flex-start',
+    marginTop: 1,
+  },
+});
